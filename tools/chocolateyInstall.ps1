046944371878952eca4559e470dfa9d2d797befb
Francesco Sacchi <depsir@gmail.com>
2014-11-13 21:16:42 +0100

    Initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'rdiff-backup'
+$url = 'http://savannah.nongnu.org/download/rdiff-backup/rdiff-backup-1.2.8-win32.zip'
+$validExitCodes = @(0)
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
